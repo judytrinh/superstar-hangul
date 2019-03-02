6209d4893764d0a91d7963120952c0c0b98fe33d
Judy Trinh <jtrinh74@gmail.com>
2019-03-01 22:32:52 -0800

    Create component for lyric entry with dummy data
    Also hooked up screens to be able to be cycled through using space or enter.
    This is temporary for most screens. It just allows us to test for now.

--- src/screens/GameplayScreen.jsx
+++ src/screens/GameplayScreen.jsx
@@ -1,17 +1,65 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import LyricEntry from './LyricEntry';
 
 /**
  * Where the actual gameplay happens. Houses the game logic, typing view, etc.
  */
 export default class GameplayScreen extends Component {
-  state = { name: 'gameplay screen' };
+  constructor() {
+    super();
+    this.state = {
+      currentTranslation: 'I miss you',
+      currentLyric: '보고 십다',
+    };
+  }
+
+  // TODO: Remove componentDidMount, componentWillUnmount, onKeyDown
+  // when we've actually hooked up the proper condition to move to next screen.
+  // This is here to be able to test cycling through screens for now.
+  componentDidMount() {
+    document.addEventListener('keydown', this.onKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.onKeyDown);
+  }
+
+  onKeyDown = (event) => {
+    if (event.keyCode !== GameplayScreen.ENTER_KEY
+        && event.keyCode !== GameplayScreen.SPACE_KEY) {
+      return;
+    }
+    const { moveToNextScreen } = this.props;
+    moveToNextScreen();
+  };
+
+  // TODO: once we nail down how the lyric + translation data will be stored,
+  // we should use that to populate the next set of lyrics
+  moveToNextLyric = () => {
+    this.setState({
+      currentTranslation: 'When I say that',
+      currentLyric: '이렇게 말하니까',
+    });
+  };
 
   render() {
-    const { name } = this.state;
+    const { currentTranslation, currentLyric } = this.state;
     return (
       <div id="gameplay-screen">
-        {name}
+        <LyricEntry
+          moveToNextLyric={this.moveToNextLyric}
+          translation={currentTranslation}
+          lyric={currentLyric}
+        />
       </div>
     );
   }
 }
+
+GameplayScreen.ENTER_KEY = 13;
+GameplayScreen.SPACE_KEY = 32;
+
+GameplayScreen.propTypes = {
+  moveToNextScreen: PropTypes.func.isRequired,
+};
--- src/screens/LyricEntry.jsx
+++ src/screens/LyricEntry.jsx
@@ -6,26 +6,52 @@ import PropTypes from 'prop-types';
  * validation and visual feedback to the user.
  */
 export default class LyricEntry extends Component {
-  state = { name: 'lyric entry' };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  // TODO: Remove both of these event listener sub/unsub calls when we actually
+  // implement moving to next set of lyrics after validation.
+  componentDidMount() {
+    document.addEventListener('click', this.validateInput, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.validateInput, false);
+  }
+
+  // Call this every time the input changes to validate whether the user has
+  // successfully typed the whole lyric
+  validateInput = () => {
+    const { moveToNextLyric } = this.props;
+    // TODO: Do some validation
+    const valid = true;
+    if (valid) {
+      moveToNextLyric();
+    }
+  };
 
   render() {
-    const { name } = this.state;
     const { translation, lyric } = this.props;
     return (
       <div className="lyric-entry">
-        {name}
         <div>{translation}</div>
         <div>{lyric}</div>
+        <input />
       </div>
     );
   }
 }
 
 LyricEntry.propTypes = {
-  translation: PropTypes.string.isRequired,
-  lyric: PropTypes.string,
+  // if in English, not required
+  translation: PropTypes.string,
+  lyric: PropTypes.string.isRequired,
+  // Call this when we've validated that the user's input is correct
+  moveToNextLyric: PropTypes.func.isRequired,
 };
 
 LyricEntry.defaultProps = {
-  lyric: '',
+  translation: '',
 };
