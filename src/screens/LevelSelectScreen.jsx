52a912829633e60514c9ebe6d1cad7440ea5ee36
louisez <lzhou.star@gmail.com>
2019-03-20 23:42:38 +0900

    added new song
    added IU palette, converted songData into songDataList and songInfo into songInfoList

--- src/screens/LevelSelectScreen.jsx
+++ src/screens/LevelSelectScreen.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {
   Card, Button, CardImg, CardTitle, CardDeck, CardBody,
 } from 'reactstrap';
-import songData from './songData';
+import songDataList from './songData';
 
 /**
  * Select the song to play.
@@ -11,23 +11,56 @@ import songData from './songData';
 export default class LevelSelectScreen extends Component {
   constructor() {
     super();
-    this.songInfo = { audioFile: '', parts: [] };
+    this.songInfoList = [{ audioFile: '', parts: [] }, { audioFile: '', parts: [] }];
   }
 
-  // Puts JSON data into songInfo to be passed to new GameplayScreen
+//  // Puts JSON data into songInfo to be passed to new GameplayScreen
+//  componentDidMount() {
+//    // songInfo contains an array of objects
+//    this.songInfo = {
+//      audioFile: 'src/audio/BTS V _ J-HOPE - HUG ME 안아줘.mp3',
+//      parts:
+//        songData.map(songInfo => ({
+//          line: songInfo.line,
+//          lyric: songInfo.korean,
+//          translation: songInfo.english,
+//          duration: Number(new Date(songInfo.stop)) - Number(new Date(songInfo.start)),
+//        })),
+//    };
+//  }
+  
+  
+  
+    // Puts JSON data into songInfo to be passed to new GameplayScreen
   componentDidMount() {
     // songInfo contains an array of objects
-    this.songInfo = {
-      audioFile: 'src/audio/BTS V _ J-HOPE - HUG ME 안아줘.mp3',
-      parts:
-        songData.map(songInfo => ({
-          line: songInfo.line,
-          lyric: songInfo.korean,
-          translation: songInfo.english,
-          duration: Number(new Date(songInfo.stop)) - Number(new Date(songInfo.start)),
-        })),
-    };
+    console.log(songDataList[1]);
+    this.songInfoList = [
+      {
+        audioFile: 'src/audio/BTS V _ J-HOPE - HUG ME 안아줘.mp3',
+        parts:
+          songDataList[0].map(songInfoList => ({
+            line: songInfoList.line,
+            lyric: songInfoList.korean,
+            translation: songInfoList.english,
+            duration: Number(new Date(songInfoList.stop)) - Number(new Date(songInfoList.start)),
+          })),
+      },
+      {
+        audioFile: 'src/audio/IU-palette.mp3',
+        parts:
+          songDataList[1].map(songInfoList => ({
+            line: songInfoList.line,
+            lyric: songInfoList.korean,
+            translation: songInfoList.english,
+            duration: Number(new Date(songInfoList.stop)) - Number(new Date(songInfoList.start)),
+          })),
+      }
+    ];
+    console.log(this.songInfoList);
   }
+
+  
 
   render() {
     const { moveToGameplayScreen } = this.props;
@@ -42,15 +75,15 @@ export default class LevelSelectScreen extends Component {
             <CardImg top width="100%" src="src/images/hugMe.jpg" alt="Card image cap" />
             <CardBody>
               <CardTitle>V, J-hope - 안아줘 (Hug me)</CardTitle>
-              <Button outline color="info" onClick={() => moveToGameplayScreen(this.songInfo)}>Easy</Button>
+              <Button outline color="info" onClick={() => moveToGameplayScreen(this.songInfoList[0])}>Easy</Button>
             </CardBody>
           </Card>
 
           <Card>
-            <CardImg top width="100%" src="src/images/laVieEnRose.jpg" alt="Card image cap" />
+            <CardImg top width="100%" src="src/images/palette.jpg" alt="Card image cap" />
             <CardBody>
-              <CardTitle>IZ*ONE - La Vie en Rose</CardTitle>
-              <Button outline color="info">Medium</Button>
+              <CardTitle>IU - Palette</CardTitle>
+              <Button outline color="info" onClick={() => moveToGameplayScreen(this.songInfoList[1])}>Medium</Button>
             </CardBody>
           </Card>
 
