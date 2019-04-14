2edfc07ca6d2e8548fafc7ebfdf0b1626e43450e
Judy Trinh <jtrinh74@gmail.com>
2019-04-13 22:13:57 -0700

    Turn off react/destructuring-assignment rule
    This rule is a bit strict imo and leads to
    cluttered code at times. Prefer to have the clarity
    of seeing where variables originate but also
    ability to use destructing when things get too verbose.

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -3,7 +3,24 @@ module.exports = {
     root: __dirname,
   },
   use: [
-    '@neutrinojs/airbnb',
+    [
+      '@neutrinojs/airbnb',
+      {
+        eslint: {
+          // For supported options, see:
+          // https://github.com/webpack-contrib/eslint-loader#options
+          // https://eslint.org/docs/developer-guide/nodejs-api#cliengine
+          // The options under `baseConfig` correspond to those
+          // that can be used in an `.eslintrc.*` file.
+          baseConfig: {
+            rules: {
+              'babel/semi': 'off',
+              'react/destructuring-assignment': 'off'
+            },
+          },
+        },
+      }
+    ],
     [
       '@neutrinojs/react',
       {
